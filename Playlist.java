1ffeae2309bfb2c1e967316e592f65227a8b0615
Atske <eyuela.fekade21@gmail.com>
2025-08-20 13:53:56 +0300

    a new variable in song called songpath and the addsong method in playlist

--- Playlist.java
+++ Playlist.java
@@ -5,12 +5,16 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Playlist {
     private Node head, tail, current;
     private String name;
     public Playlist(String name){
         this.name = name;
+        this.head = null;
+        this.tail = null;
+        this.current = null;
         Path folderPath = Paths.get(name);
         try {
             if(!Files.exists(folderPath)){
@@ -22,13 +26,32 @@ public class Playlist {
             e.printStackTrace();
         }
     }
+
     public void addSong(){
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Select a Mp3 File");
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("MP3 Files", "mp3");
+        fileChooser.setFileFilter(filter);
+
         int result = fileChooser.showOpenDialog(null);
+
         if (result == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             System.out.println("File selected: " + selectedFile.getAbsolutePath());
+            
+            //set up input window
+            Song song = new Song();
+            Node newnode = new Node(song);
+            if(head == null){
+                head = newnode;
+                tail = newnode;
+                current = head;
+            }else{
+                tail.next = newnode;
+                newnode.prev = tail;
+                tail = newnode;
+            }
         } else {
             System.out.println("No file selected.");
         }
--- Song.java
+++ Song.java
@@ -1,15 +1,20 @@
 public class Song {
-    private String title, artist;
+    private String title, artist, songPath;
     private double duration;
     public Song(){
         this.title = "Unknown";
         this.artist = "Unknown";
+        this.songPath = "Unknown";
         this. duration = 0.00;
     }
-    public Song(String title, String artist, double duration){
+    public Song(String title, String artist, String songPath, double duration){
         this.title = title;
         this.artist = artist;
+        this.songPath = songPath;
         this. duration = duration;
+    }
+    public String getSongPathString(){
+        return songPath;
     }
     public String getTitleString(){
         return title;
@@ -19,6 +24,16 @@ public class Song {
     }
     public double getduration(){
         return duration;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public void setArtist(String artist){
+        this.artist = artist;
+    }
+    public void setDuration(double duration){
+        this.duration = duration;
     }
 
 }
